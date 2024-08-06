fde685f879e5048367038bffe544774ffeeb0ceb
Cfun1 <15718354+Cfun1@users.noreply.github.com>
2024-08-05 21:29:37 -0400

    Create a helper class that holds exception code number to reduce dev mistake

--- CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -23,15 +23,16 @@ public class ApplicantEducationLogic : BaseLogic<ApplicantEducationPoco>
         {
             if (string.IsNullOrEmpty(poco.Major) || poco.Major.Length < 3)
                 validationExceptions.Add(
-                    new ValidationException(107, "Cannot be empty or less than 3 characters"));
+                    new ValidationException(ExceptionCodes.ApplicantEducation_Major, "Cannot be empty or less than 3 characters"));
 
             if (poco.StartDate > DateTime.Now)
                 validationExceptions.Add(
-                    new ValidationException(108, $"{poco.StartDate} Cannot be greater than today's date {DateTime.Now}"));
+                    new ValidationException(ExceptionCodes.ApplicantEducation_StartDate,
+                    $"{poco.StartDate} Cannot be greater than today's date {DateTime.Now}"));
 
             if (poco.CompletionDate < poco.StartDate)
                 validationExceptions.Add(
-                    new ValidationException(109, $"{poco.CompletionDate} CompletionDate cannot be earlier than StartDate {poco.StartDate}"));
+                    new ValidationException(ExceptionCodes.ApplicantEducation_CompletionDate, $"{poco.CompletionDate} CompletionDate cannot be earlier than StartDate {poco.StartDate}"));
         }
 
         if (validationExceptions.Count > 0)
