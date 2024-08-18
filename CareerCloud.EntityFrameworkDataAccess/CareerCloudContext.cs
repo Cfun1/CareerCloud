2b619303cedf41bf11bf0a0270717658dc3d8683
Cfun1 <15718354+Cfun1@users.noreply.github.com>
2024-08-17 20:57:46 -0400

    initial db sets

--- CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -1,18 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CareerCloud.Pocos;
+using Microsoft.EntityFrameworkCore;
 
 namespace CareerCloud.EntityFrameworkDataAccess;
 
 public class CareerCloudContext : DbContext
 {
+    public DbSet<ApplicantEducationPoco> ApplicantEducation;
+
+    public DbSet<ApplicantJobApplicationPoco> ApplicantJobApplication;
+
+    public DbSet<ApplicantProfilePoco> ApplicantProfile;
+
+    public DbSet<ApplicantResumePoco> ApplicantResume;
+
+    public DbSet<ApplicantSkillPoco> ApplicantSkill;
+
+    public DbSet<ApplicantWorkHistoryPoco> ApplicantWorkHistory;
+
+    public DbSet<CompanyDescriptionPoco> CompanyDescription;
+
+    public DbSet<CompanyJobDescriptionPoco> CompanyJobDescription;
+
+    public DbSet<CompanyJobEducationPoco> CompanyJobEducation;
+
+    public DbSet<CompanyJobPoco> CompanyJob;
+
+    public DbSet<CompanyJobsDescriptionPoco> CompanyJobsDescription;
+
+    public DbSet<CompanyJobSkillPoco> CompanyJobSkill;
+
+    public DbSet<CompanyLocationPoco> CompanyLocation;
+
+    public DbSet<CompanyProfilePoco> CompanyProfile;
+
+    public DbSet<SecurityLoginPoco> SecurityLogin;
+
+    public DbSet<SecurityLoginsLogPoco> SecurityLoginsLog;
+
+    public DbSet<SecurityLoginsRolePoco> SecurityLoginsRole;
+
+    public DbSet<SecurityRolePoco> SecurityRole;
+
+    public DbSet<SystemCountryCodePoco> SystemCountryCode;
+
+    public DbSet<SystemLanguageCodePoco> SystemLanguageCode;
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(DataAccessLayer.CommonDbConnection.String);
-        base.OnConfiguring(optionsBuilder);
     }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        /* Describe later foreign keys relationship*/
     }
 }
